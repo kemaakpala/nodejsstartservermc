cc5e3099085dce07886ad4dbe1916cfe164134a0
Patrick Akpala <patrick.akpala@lloydsbanking.com>
2021-07-23 16:44:23 +0100

    parsing request paths

--- index.js
+++ index.js
@@ -4,13 +4,26 @@
 
 // Dependencies
 var http = require('http');
+var url = require('url');
 
 // The server should respond to all requests with a string
 var server = http.createServer(function(req, res) {
+  // Get the url and parse it
+  var parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g,'')
+
+  // Send the response
   res.end('Hello World\n');
+
+  // Log the request path
+  console.log('Request received on this path: '+trimmedPath)
+  
 });
 
 // Start the server, and have it listen on port 3000
 server.listen(3000, function(){
-  console.log("The server is listening on port 3000");
+  console.log("The server is listening on port 3000 now");
 })
